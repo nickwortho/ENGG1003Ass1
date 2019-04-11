5710bc1a320b8a586e6514b61af7bdb50c2a531c
Nick Worthington <nickrworthington@gmail.com>
2019-04-11 21:38:00 +1000

    pointer edits

--- Encryption.c
+++ Encryption.c
@@ -1,39 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-char msgEncrypt(char message[], int key);
+char *cEncrypt(char *plainText, int key);
 
 int main() {
 
   char x;
-  //int key = 1; // Single integer encryption key in the range [0,25]
-  //int index;
-  //char message[] = {"ATTACK AT SUNRISE"}; // Plain text message to be manipulated
-
-  x = msgEncrypt("the number eight", 1);
+  int key = 1; // Single integer encryption key in the range [0,25]
+  char plainText[100] = {"ATTACK AT SUNRISE"}; // Plain text message to be manipulated
 
 
 // Prints cipher text version of message
-  printf("%c\n", x);
+  printf("%s\n", *cEncrypt(plainText, key));
   return 0;
 }
 
 
 // CAESAR ENCRYPTION
 //
-char msgEncrypt(char message[], int key) {
+char *cEncrypt(char *plainText, int key) {
+  char cipherText[100];
 
-    for (int index = 0; index < 17; index++) {
-
-  // Converts any lower case letters to upper case
-      if (message[index] <= 122 && message[index] >= 97) {
-        message[index] -= 32;
-      }
+  for (int index = 0; plainText[index] != '\0'; index++) {
+    // Converts any lower case letters to upper case
+    if ((int)plainText[index] <= 122 && (int)plainText[index] >= 97) {
+      plainText[index] -= 32;
+        }
 
   // Converts each plain text char into a cipher text char in message
-      if (message[index] >= 65 && message[index] <= 90) {
-        message[index] = ((message[index] + key - 65) % 26 + 65);
-      }
+    if ((int)plainText[index] >= 65 && (int)plainText[index] <= 90) {
+      cipherText[index] = (char)((int)plainText[index] - 65 + key) % 26 + 65;
     }
-  return *message;
+  }
+  return *cipherText;
 }
